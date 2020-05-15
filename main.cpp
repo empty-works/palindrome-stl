f874189469065ad365e7572869e55e9ef4279b68
empty-works <pama.mark@gmail.com>
2020-05-15 20:46:55 +0200

    Started the project and initialized needed variables.

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <iomanip>
+#include <deque>
+#include <string>
+#include <vector>
+#include <cctype>
+
+bool is_palindrome(const std::string &s) {
+		
+}
+
+int main {
+	std::vector<std::string> test_strings {"a", "aa", "aba", "abba", "abbcbba", "ab", "abc", "radar", "bob", "ana", 
+		"avid diva", "Amore, Roma", "A Toyota's a toyota", "A Santa at NASA", "C++",
+		"A man, a plan, a cat, a ham, a yak, a yam, a hat, a canal-Panama!", "This is a palindrome", "palindrome"};
+	
+	std::cout << std::boolalpha;
+	std::cout << std::setw(8) << std::left << "Result" << "String" << std::endl;
+	for(const auto &s : test_strings) {
+		std::cout << std::setw(8) << std::left << is_palindrome(s) << std::endl;
+	}
+	std::cout << std::endl;
+	return 0;
+}
